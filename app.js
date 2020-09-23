09410945a9c35fc228b986a27ae215883741c230
Timofey Chunarev <timofei.chunarev@gmail.com>
2020-09-23 18:53:08 +0700

    Connected to the database

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
+mongoose.connect('mongodb+srv://timofei-chunarev:0918273645az@cluster0.uxhcw.mongodb.net/Cluster0?retryWrites=true&w=majority', { useNewUrlParser: true })
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
